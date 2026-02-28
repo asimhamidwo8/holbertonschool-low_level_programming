a1d21f5c5f039fbc9b2a5da36e9ad71b1a1c54a4
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-02-28 21:50:24 +0000

    Add print_to_98 function

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,6 +6,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
 void times_table(void);
+void print_to_98(int n);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
