75f08d6f4e9540bbd22921bed47c70a9450d561d
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-02-28 21:38:04 +0000

    Add times_table prototype

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
+void times_table(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
