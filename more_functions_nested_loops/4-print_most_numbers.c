471107dbb8f573eaabebd3e13959c4aed3515379
asimhamudwo8 <asimhamudwo8@gmail.com>
2026-04-12 15:26:57 +0000

    Fix Betty doc

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
 void print_most_numbers(void)
 {
 	int i;
@@ -7,9 +12,7 @@ void print_most_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
-		{
 			_putchar(i + '0');
-		}
 	}
 	_putchar('\n');
 }
