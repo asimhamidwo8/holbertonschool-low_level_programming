26e1a4f602422accc49df0e21eea0b70d9324e22
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-04-25 19:25:11 +0000

    Add _strstr function

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -3,16 +3,17 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-	char str[] = "Look up!\n";
-	char *ptr;
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
 
-	ptr = string_toupper(str);
-	printf("%s", ptr);
-	printf("%s", str);
+	t = _strstr(s, f);
+	printf("%s\n", t);
 
 	return (0);
 }
