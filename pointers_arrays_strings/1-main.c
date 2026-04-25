f613d89c01d3ad7d3e4f2305e0fd9f26de055524
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-04-25 19:00:28 +0000

    Add _memcpy function

--- pointers_arrays_strings/1-main.c
+++ pointers_arrays_strings/1-main.c
@@ -2,28 +2,42 @@
 #include <stdio.h>
 
 /**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: memory address
+ * @size: size of memory
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i % 10)
+			printf(" ");
+		if (i % 10 == 0 && i)
+			printf("\n");
+		printf("0x%02x", buffer[i]);
+	}
+	printf("\n");
+}
+
+/**
  * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
+	char buffer[98] = {0};
+	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-	printf("%s\n", s1);
-	printf("%s", s2);
+	simple_print_buffer(buffer, 98);
 
-	ptr = _strncat(s1, s2, 1);
-	printf("%s\n", s1);
-	printf("%s", s2);
-	printf("%s\n", ptr);
+	_memcpy(buffer + 50, buffer2, 10);
 
-	ptr = _strncat(s1, s2, 1024);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
+	printf("-------------------------------------------------\n");
+
+	simple_print_buffer(buffer, 98);
 
 	return (0);
 }
