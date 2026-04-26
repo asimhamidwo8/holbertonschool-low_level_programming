9896139e006708c626c1f92647c134ce32b999e2
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-04-26 10:05:35 +0000

    0. create array with malloc

--- argc_argv/main.c
+++ argc_argv/main.c
@@ -1,11 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * main - check the code
- * Return: Always 0
- */
 int main(void)
 {
-    return (0);
+	char *buffer;
+	unsigned int i;
+
+	buffer = create_array(10, 'H');
+
+	if (buffer == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+
+	for (i = 0; i < 10; i++)
+		printf("%c", buffer[i]);
+
+	printf("\n");
+
+	free(buffer);
+
+	return (0);
 }
