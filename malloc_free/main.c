0278d73e2fe8e9ae59016d8d5d878a8aaa93dad9
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-04-26 11:10:34 +0000

    0. malloc_checked

--- malloc_free/main.c
+++ malloc_free/main.c
@@ -2,47 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
- * @width: width of the grid
- * @height: height of the grid
- */
-void print_grid(int **grid, int width, int height)
-{
-	int w, h;
-
-	for (h = 0; h < height; h++)
-	{
-		for (w = 0; w < width; w++)
-			printf("%d ", grid[h][w]);
-		printf("\n");
-	}
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0
- */
 int main(void)
 {
-	int **grid;
+	char *c;
 
-	grid = alloc_grid(6, 4);
+	c = malloc_checked(sizeof(char) * 10);
+	printf("Allocated!\n");
 
-	if (grid == NULL)
-		return (1);
-
-	print_grid(grid, 6, 4);
-	printf("\n");
-
-	grid[0][3] = 98;
-	grid[3][4] = 402;
-
-	print_grid(grid, 6, 4);
-
-	free_grid(grid, 4);
-
+	free(c);
 	return (0);
 }
