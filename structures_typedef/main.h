189ab374f77a9b4e23ccd96a86214963e4ca8f20
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-04-27 06:37:31 +0000

    task 2: print dog struct

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -16,13 +16,8 @@ struct dog
 	char *owner;
 };
 
-/**
- * init_dog - initialize a struct dog
- * @d: pointer to struct dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
+/* prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
