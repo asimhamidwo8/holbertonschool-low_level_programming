df91e71004e59f839dffad56b2de6b9b6082dff0
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-04-27 09:58:13 +0000

    3. simple calculator using function pointers

---
+++ function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - calculator program
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((argv[2][0] == '/' && b == 0) ||
+	    (argv[2][0] == '%' && b == 0))
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	printf("%d\n", func(a, b));
+	return (0);
+}
