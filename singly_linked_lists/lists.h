ccc9371e9234e57fc89a3af760f1ccadd4b4bd7e
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-04-28 08:29:08 +0000

    2. add node at beginning

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,9 +5,9 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @str: string
  * @len: length of string
- * @next: points to next node
+ * @next: pointer to next node
  */
 typedef struct list_s
 {
@@ -18,5 +18,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
