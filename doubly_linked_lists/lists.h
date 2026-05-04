bca038c44aa14775bbef8f4958d36d300100617c
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-05-04 14:54:36 +0000

    Task 0 and Task 1: doubly linked list functions

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -11,12 +11,13 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
-/* prototype */
+/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
-#endif
+#endif /* LISTS_H */
