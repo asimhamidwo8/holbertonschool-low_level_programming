6b55e850bfedf486e647ceca5514d651ee63356e
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-05-07 22:57:38 +0000

    Add function to sum all nodes in doubly linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all data of a dlistint_t list
+ * @head: pointer to the head of the list
+ * Return: sum of all nodes data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
