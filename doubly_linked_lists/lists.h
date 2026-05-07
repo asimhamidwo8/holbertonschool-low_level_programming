aaf158681a918e08a8b4b386c5daaaf1d6da6d9d
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-05-07 22:45:09 +0000

    Add function to free doubly linked list

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -4,10 +4,11 @@
 #include <stddef.h>
 
 /**
- * struct dlistint_s - doubly linked list node
+ * struct dlistint_s - doubly linked list
  * @n: integer
- * @prev: pointer to previous node
- * @next: pointer to next node
+ * @prev: points to the previous node
+ * @next: points to the next node
+ * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
@@ -16,10 +17,8 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 
-#endif /* LISTS_H */
+#endif
