9c15b7b5b0ad8d0f6f20172698f397fb5fc32185
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-05-07 23:24:00 +0000

    Add delete node at index function for doubly linked list

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at a given index
+ * @head: pointer to head pointer
+ * @index: index of the node to delete (starts at 0)
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	tmp = *head;
+
+	if (index == 0)
+	{
+		*head = tmp->next;
+
+		if (*head != NULL)
+			(*head)->prev = NULL;
+
+		free(tmp);
+		return (1);
+	}
+
+	while (tmp != NULL && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (tmp == NULL)
+		return (-1);
+
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
+
+	if (tmp->prev != NULL)
+		tmp->prev->next = tmp->next;
+
+	free(tmp);
+
+	return (1);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,6 +3,12 @@
 
 #include <stddef.h>
 
+/**
+ * struct dlistint_s - doubly linked list node
+ * @n: integer value
+ * @prev: pointer to previous node
+ * @next: pointer to next node
+ */
 typedef struct dlistint_s
 {
 	int n;
@@ -17,5 +23,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
